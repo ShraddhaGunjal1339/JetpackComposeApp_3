3f3d5575b4f16da5d6f4e2db6d719d063f601139
Shraddha <shraddhagunjal1037@gmail.com>
2024-01-07 22:52:55 +0530

    Added String Resource

--- app/src/main/java/com/example/jetpack_compose_tutorial_app/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_tutorial_app/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,9 +33,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingPage(
-                        title = "Jetpack Compose tutorial",
-                        message1 = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                        message2 = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.")
+                        title = stringResource(R.string.jetpack_compose_tutorial),
+                        message1 = getString(R.string.message_1),
+                        message2 = getString(R.string.message_2))
                     //Greeting("Android")
                 }
             }
